3cb4aef50d313d5123d7514cd4ac6244df7189fb
vytautaspuplesis <76166684+vytautaspuplesis@users.noreply.github.com>
2020-12-17 16:03:24 +0200

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string str; 
+           //int l= 0;
+           Console.Write("Input a string : ");
+           str = Console.ReadLine();
+           if (str.Length>=1)
+           {
+           var s = str.Substring(0,1);
+           Console.WriteLine(s + str + s);
+           }
+        }
+    }
+}
