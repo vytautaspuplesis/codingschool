c2b7436f79fdba031c0ffad1d33b11f78d207f0f
vytautaspuplesis <76166684+vytautaspuplesis@users.noreply.github.com>
2020-12-17 16:07:42 +0200

    Add files via upload

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your text");
+            string str = Console.ReadLine();
+            string res = string.Join(" ", str.Split(' ').Select(s => new String(s.Reverse().ToArray())));
+            Console.WriteLine(res);
+
+        }
+    }
+}
